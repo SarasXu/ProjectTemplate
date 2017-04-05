4fee7ce2476a387ee85246db970d1e65e39ce590
SarasXu <saras_xu@163.com>
2017-04-05 09:06:54 +0800

    增加redis配置

--- template/template-common/src/main/java/com/saras/template/config/RedisTemplateConfig.java
+++ template/template-common/src/main/java/com/saras/template/config/RedisTemplateConfig.java
@@ -1,91 +1,56 @@
-//package com.saras.template.config;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.cache.annotation.CachingConfigurerSupport;
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.core.StringRedisTemplate;
-//import redis.clients.jedis.JedisPool;
-//import redis.clients.jedis.JedisPoolConfig;
-//
-///**
-// * redis配置
-// */
-//@Configuration
-//@EnableAutoConfiguration
-//@EnableCaching
-//public class RedisTemplateConfig extends CachingConfigurerSupport {
-//	private final static Logger logger = LoggerFactory.getLogger(RedisTemplateConfig.class);
-//
-//
-//    @Bean(name= "jedis.pool")
-//    public JedisPool jedisPool(@Qualifier("jedis.pool.config") JedisPoolConfig config,
-//                               @Value("${jedis.pool.host}")String host,
-//                               @Value("${jedis.pool.port}")int port) {
-//        return new JedisPool(config, host, port);
-//    }
-//
-//
-//    @Bean(name= "jedis.pool.config")
-//    public JedisPoolConfig jedisPoolConfig (@Value("${jedis.pool.config.maxTotal}")int maxTotal,
-//                                            @Value("${jedis.pool.config.maxIdle}")int maxIdle,
-//                                            @Value("${jedis.pool.config.maxWaitMillis}")int maxWaitMillis) {
-//        JedisPoolConfig config = new JedisPoolConfig();
-//        config.setMaxTotal(maxTotal);
-//        config.setMaxIdle(maxIdle);
-//        config.setMaxWaitMillis(maxWaitMillis);
-//        return config;
-//    }
-//
-//    @Bean
-//	public JedisConnectionFactory getConnectionFactory() {
-//		JedisConnectionFactory factory = new JedisConnectionFactory();
-//		JedisPoolConfig config = new JedisPoolConfig();
-//		config.setMaxIdle(8);
-//		config.setMinIdle(0);
-//		config.setMaxWaitMillis(10);
-//		factory.setPoolConfig(config);
-//		logger.info("JedisConnectionFactory bean init success.");
-//		return factory;
-//	}
-//
-//	@Bean
-//	public RedisTemplate<?, ?> getRedisTemplate() {
-//		return new StringRedisTemplate(getConnectionFactory());
-//	}
-////    @Bean
-////    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-////
-////        RedisTemplate<String,String> redisTemplate = new RedisTemplate<String, String>();
-////
-////        redisTemplate.setConnectionFactory(factory);
-////
-////
-////
-////        //key序列化方式;（不然会出现乱码;）,但是如果方法上有Long等非String类型的话，会报类型转换错误；
-////
-////        //所以在没有自己定义key生成策略的时候，以下这个代码建议不要这么写，可以不配置或者自己实现ObjectRedisSerializer
-////
-////        //或者JdkSerializationRedisSerializer序列化方式;
-////
-//////           RedisSerializer<String>redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
-////
-//////           redisTemplate.setKeySerializer(redisSerializer);
-////
-//////           redisTemplate.setHashKeySerializer(redisSerializer);
-////
-////
-////
-////        return redisTemplate;
-////
-////    }
-//}
+package com.saras.template.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * redis配置
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableCaching
+public class RedisTemplateConfig {
+    private final static Logger logger = LoggerFactory.getLogger(RedisTemplateConfig.class);
+
+    @Bean
+    public JedisConnectionFactory getConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(50);
+        config.setMinIdle(0);
+        config.setMaxWaitMillis(1000);
+        config.setMaxTotal(500);
+        factory.setPassword("root123");
+        factory.setHostName("127.0.0.1");
+        factory.setPort(6379);
+        factory.setTimeout(1000);
+        factory.setDatabase(1);
+        factory.setPoolConfig(config);
+        logger.info("JedisConnectionFactory bean init success.");
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> getRedisTemplate() {
+        return new StringRedisTemplate(getConnectionFactory());
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+        //设置缓存过期时间 可根据需求设置
+        //rcm.setDefaultExpiration(60);//秒
+        return rcm;
+
+    }
+}
--- template/template-web/src/main/java/com/saras/template/TestController.java
+++ template/template-web/src/main/java/com/saras/template/TestController.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import com.saras.template.config.ConfigProperty;
 import com.saras.template.entity.User;
 import com.saras.template.mapper.UserMapper;
-import com.saras.template.service.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +16,7 @@ import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -25,72 +25,83 @@ import java.util.List;
  */
 @Controller
 public class TestController {
-	private final static Logger logger = LoggerFactory.getLogger(TestController.class);
-	@Autowired
-	private ConfigProperty configProperty;
-	@Autowired
-	private UserMapper userMapper;
-	@Autowired
-	private ThreadPoolTaskExecutor taskExecutor;
-	@Autowired
-	private TransactionTemplate transactionTemplate;
-//	@Autowired
-//	private RedisTemplate redisTemplate;
-	
-	@RequestMapping("hello")
-	public String hello(Model model) {
-		List<String> list = Lists.newArrayList();
-		list.add("hello");
-		list.add(configProperty.getName());
-		list.add("velocity");
-		doBiz("no");
-//		ValueOperations<String,String> valueOperations = redisTemplate.opsForValue();
-//		valueOperations.set("one","667787889");
-		User user = userMapper.selectByPrimaryKey(1l);
-		taskExecutor.execute(() -> logger.info("这里是线程池中的多线程"));
-		logger.info("user：{}", user);
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+    @Autowired
+    private ConfigProperty configProperty;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("hello")
+    public String hello(Model model) {
+        List<String> list = Lists.newArrayList();
+        list.add("hello");
+        list.add(configProperty.getName());
+        list.add("velocity");
+        doBiz("no");
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("yyyyyyyuuuuuu", "667787889");
+        User user = userMapper.selectByPrimaryKey(1l);
+        taskExecutor.execute(() -> logger.info("这里是线程池中的多线程"));
+        logger.info("user：{}", user);
 //		logger.info(valueOperations.get("one"));
-		model.addAttribute("list", list);
-		logger.info("hello info：{}", list);
-		logger.warn("hello warn：{}", list);
-		logger.error("hello error：{}", list);
-		return "hello";
-	}
-	
-	protected void doBiz(String cmd) {
-		Biz biz = new Biz(cmd);
-		transactionTemplate.execute(biz);
-	}
-	
-	private class Biz implements TransactionCallback<Void> {
-		private String cmd;
-		
-		public Biz(String cmd) {
-			this.cmd = cmd;
-		}
-		
-		@Override
-		public Void doInTransaction(TransactionStatus transactionStatus) {
-			try {
-				logger.info("启动编程式事务管理");
-				User user = new User();
-				String value = "taiajaja";
-				user.setDdd(value);
-				user.setUser_id(value);
-				user.setUser_name(value);
-				user.setMemo(value);
-				user.setPwd(value);
-				userMapper.insert(user);
-				
-				if ("back".equals(cmd)) {
-					throw new IllegalStateException("测试回滚");
-				}
-			} catch (Exception e) {
-				transactionStatus.setRollbackOnly();
-				logger.info("回滚插入");
-				//				throw e;
-			}
-			return null;
-		}
-	}
+        model.addAttribute("list", list);
+        logger.info("hello info：{}", list);
+        logger.warn("hello warn：{}", list);
+        logger.error("hello error：{}", list);
+        return "hello";
+    }
+
+    protected void doBiz(String cmd) {
+        Biz biz = new Biz(cmd);
+        transactionTemplate.execute(biz);
+    }
+
+    private class Biz implements TransactionCallback<Void> {
+        private String cmd;
+
+        public Biz(String cmd) {
+            this.cmd = cmd;
+        }
+
+        @Override
+        public Void doInTransaction(TransactionStatus transactionStatus) {
+            try {
+                logger.info("启动编程式事务管理");
+                User user = new User();
+                String v = (String) redisTemplate.opsForValue().get("one");
+                String value = "taiajaja";
+                user.setDdd(value);
+                user.setUser_id(value);
+                user.setUser_name(value);
+                user.setMemo(value);
+                user.setPwd(value);
+                userMapper.insert(user);
+
+                if ("back".equals(cmd)) {
+                    throw new IllegalStateException("测试回滚");
+                }
+            } catch (Exception e) {
+                transactionStatus.setRollbackOnly();
+                logger.info("回滚插入");
+                //				throw e;
+            }
+            return null;
+        }
+    }
+
+    @RequestMapping("act")
+    @ResponseBody
+    public Object act() {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String value = valueOperations.get("yyyyyyyuuuuuu");
+        logger.info("redis缓存应用：{}", value);
+        return value;
+    }
+
 }
